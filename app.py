0e0ff133b88ee8d446369ea788ae23fb226569ac
karankumarshreds <naileditnerd@gmail.com>
2021-02-21 22:40:00 +0530

    add routes

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ from flask import Flask
 ## __name__ gives each file a unique name 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return { "Response": "Home page" }
+@app.route('/', method=['POST'])
+def create_store():
+    return { "response": "Store created" } 
+
+@app.route('/store/<string:name>', method=['GET'])
+def get_store():
+    return { "response": "Get Store" + name } 
+
+@app.route('/store')
+def get_stores():
+    return { "response": "All stores" }
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item():
+    pass 
+
+@app.route('/store/<string:name>/item')
+def get_item():
+    pass
 
 app.run(port=5000)
