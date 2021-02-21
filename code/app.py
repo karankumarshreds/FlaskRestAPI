c961322059e47fe44f676576aabb7757e22906da
karankumarshreds <naileditnerd@gmail.com>
2021-02-22 02:25:20 +0530

    fixed resource

--- code/app.py
+++ code/app.py
@@ -5,10 +5,23 @@ app = Flask(__name__)
 api = Api(app)
 ## api works with resources 
 
-class Student(Resource):
+## dummy data
+items = []
+
+class Item(Resource):
+    ## get a specific item
     def get(self, name):
-        return { "student": name }
+        item = [ item for item in items if item['name'] == name ]
+        if len(item) < 1:
+            return 
+        return { "item": name }
     
-api.add_resource(Student, '/student/<string:name>')
+    ## create a specific item
+    def post(self, name):
+        item = { 'name': name, 'price': 12 }
+        items.append(item)
+        return item
+    
+api.add_resource(Item, '/items/<string:name>')
 
 app.run(port=5000)
