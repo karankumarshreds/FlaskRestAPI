f1328c64f994066ca94dda0231e3f4b999375156
karankumarshreds <naileditnerd@gmail.com>
2021-02-22 22:53:12 +0530

    add new project setup

---
+++ with-sql/security.py
@@ -0,0 +1,26 @@
+from user import User
+
+__all__ = [
+    'authenticate',
+    'identify'
+]
+
+users = [
+    User(1, 'bob', 'passwd'),
+    User(2, 'mark', 'passwd'),
+]
+
+username_mapping = { u.username: u for u in users }
+
+userid_mapping = { u.id: u for u in users }
+
+## function to check the password 
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+## function to identify the user 
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping.get(user_id, None)
