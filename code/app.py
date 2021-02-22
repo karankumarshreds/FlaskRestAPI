a4097feee7f2232bb1904476029dfaee6df5403d
karankumarshreds <naileditnerd@gmail.com>
2021-02-22 21:16:46 +0530

    add body validation

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request 
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required 
 from security import authenticate, identity
 
@@ -52,14 +52,23 @@ class Item(Resource):
         return items
 
     def put(self, name):
-        data = request.get_json()
+        # data = request.get_json()
+        ## body validation
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'price', 
+            type=float, 
+            required=True, 
+            help="This field cannot be left blank" 
+        )
+        data = parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = { 'name': name, 'price': data['price'] }
             items.append(item)
         else:
             item.update(data)
-        return item
+        return item, 201
     
 class ItemList(Resource):
     ## get item list
