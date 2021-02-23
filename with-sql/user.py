bfefca748eb5390242b98fec27977200dd55f14d
karankumarshreds <naileditnerd@gmail.com>
2021-02-24 00:01:46 +0530

    add find username method

--- with-sql/user.py
+++ with-sql/user.py
@@ -1,5 +1,17 @@
+import sqlite3
+import os 
+
+SQL_URI = os.getenv("SQL_URI")
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    def find_by_username(self, username):
+        connection = sqlite3.connect(SQL_URI)
+        cursor = connection.cursor()
+        query = "SELECT * from users WHERE username=?"
+        result = cursor.execute(query, (username,))
+
