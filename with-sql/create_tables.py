613e56f9c012cb84d54ab2d6783591670b1388c2
karankumarshreds <naileditnerd@gmail.com>
2021-02-24 02:50:07 +0530

    update files

--- with-sql/create_tables.py
+++ with-sql/create_tables.py
@@ -7,7 +7,7 @@ connection = sqlite3.connect(SQL_URI)
 cursor = connection.cursor()
 
 ## USE : INTEGER PRIMARY KEY for auto incrementing id 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username text, password text)"
 cursor.execute(create_table)
 
 connection.commit()
