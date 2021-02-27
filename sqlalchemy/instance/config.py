0d075fbc343b3925a5246c33e9217c1061f8bdbe
karankumarshreds <naileditnerd@gmail.com>
2021-02-27 14:13:34 +0530

    add custom configs

--- sqlalchemy/instance/config.py
+++ sqlalchemy/instance/config.py
@@ -1,8 +1,24 @@
 import os
 
 class Config(object):
-    ''' Parent configuration class '''
+    ''' Parent configuration base class '''
     DEBUG = False
     CSRF_ENABLED = True 
     SECRET = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+
+class DevelopmentConfig(Config):
+    DEBUG = True 
+
+class TestingConfig(Config):
+    TESTING = True 
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI')
+    DEBUG = True
+
+class StagingConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
